5952a248d6ac17d8534b9e3f30ce82b70eec6142
Samuel Kovaka <skovaka1@jhu.edu@login-node03.cm.cluster>
2016-12-14 13:45:56 -0500

    Added header to stringtie_stdin.sh

--- stringtie_stdin.sh
+++ stringtie_stdin.sh
@@ -5,6 +5,9 @@ tmp_filename="stringtie_tmp.txt"
 rm -f $tmp_filename
 touch $tmp_filename
 
+echo "@HD   VN:1.0  SO:coordinate" >> $tmp_filename
+echo "@SQ SN:chrX LN:156040895" >> $tmp_filename
+
 while read line
 do
     echo $line >> $tmp_filename
@@ -12,4 +15,4 @@ done
 
 stringtie -p 8 $tmp_filename
 
-rm $tmp_filename
+#rm $tmp_filename
