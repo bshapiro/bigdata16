6e3d57d4cecc6cdfabe15d9753e3e45909bd41f6
RSherman15 <rsherman@g.hmc.edu>
2016-12-17 08:03:07 +0000

    Added a line to create the index for the X chromosome bam file already in the image we load

--- configureMaster.sh
+++ configureMaster.sh
@@ -1,5 +1,6 @@
 sudo cp samtools /usr/bin/
 sudo cp stringtie /usr/bin/
+samtools index /home/Rachel/ERR188044_chrX_sorted.bam
 #wget http://ccb.jhu.edu/software/stringtie/dl/stringtie-1.3.1c.Linux_x86_64.tar.gz
 #tar -zxvf stringtie-1.3.1c.Linux_x86_64.tar.gz
 #sudo mv stringtie-1.3.1c.Linux_x86_64/stringtie /usr/bin/
