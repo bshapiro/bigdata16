ec84e21007a6b80283a1b0c1f51a702200e06474
RSherman15 <rsherman@g.hmc.edu>
2016-12-17 02:43:01 +0000

    modified to fix read count estimates

--- sequential.py
+++ sequential.py
@@ -7,18 +7,20 @@ from pyspark import SparkFiles
 
 sqlContext = SQLContext(sc)
 
+sc.addPyFile('/home/Rachel/bigdata16/gtf_merge.py')
 sc.addPyFile('/home/Rachel/bigdata16/overlapParser.py')
 #sc.addFile('/home/Rachel/ERR188044_chrX_sorted.bam')
 #sc.addFile('/home/Rachel/ERR188044_chrX_sorted.bam.bai')
 sc.addFile('/home/Rachel/test.bam')
 sc.addFile('/home/Rachel/test.bam.bai')
-sc.addFile('/home/Rachel/ERR188044_chrX_reduced.bam')
-sc.addFile('/home/Rachel/ERR188044_chrX_reduced.bam.bai')
+#sc.addFile('/home/Rachel/ERR188044_chrX_reduced.bam')
+#sc.addFile('/home/Rachel/ERR188044_chrX_reduced.bam.bai')
 sc.addFile("/home/Rachel/bigdata16/stringtie_mod")
 sc.addFile("/usr/bin/stringtie")
-sc.addFile("/usr/local/bin/samtools")
+sc.addFile("/usr/bin/samtools")
 
 from overlapParser import OverlapParser
+from gtf_merge import *
 
 PRINT_DEBUG = False
 MULTIMAP_FLAG = "NH"
@@ -30,7 +32,7 @@ def rowLambda(row):
     ovr = 100
     ovr = OverlapParser(in_fname, max_gap)
     group = groups[int(row[0])]
-    return [(row[1], (samLine, group[0])) for samLine in ovr.get_group(group)]
+    return [(row[1], (samLine, group[0], row[0])) for samLine in ovr.get_group(group)]
 
 in_fname = '/home/Rachel/test.bam'
 #in_fname ='/home/Rachel/ERR188044_chrX_reduced.bam'
@@ -51,24 +53,25 @@ while True:
     groupCount += 1
     edge_tuples_strings.extend([(str(item[0]), str(item[1])) for item in cons])
 
-vertex_strings = [(str(i),) for i in  xrange(groupCount)]
-e = sqlContext.createDataFrame(edge_tuples_strings, ['src', 'dst'])
-v = sqlContext.createDataFrame(vertex_strings, ['id'])
-g = GraphFrame(v,e)
-result = g.connectedComponents()
+#vertex_strings = [(str(i),) for i in  xrange(groupCount)]
+#e = sqlContext.createDataFrame(edge_tuples_strings, ['src', 'dst'])
+#v = sqlContext.createDataFrame(vertex_strings, ['id'])
+#g = GraphFrame(v,e)
+#result = g.connectedComponents()
 
-result.rdd.saveAsPickleFile('/pickles/connCompPartitionsRDD_test.pkl')
+#result.rdd.saveAsPickleFile('/pickles/connCompPartitionsRDD_test.pkl')
 #result.rdd.saveAsPickleFile('/pickles/connCompPartitionsRDD_reduced.pkl')    
-result_rdd = result.rdd
+#result_rdd = result.rdd
 
 #result_rdd = sc.pickleFile('/pickles/connCompPartitionsRDD_reduced.pkl')   
+result_rdd = sc.pickleFile('/pickles/connCompPartitionsRDD_test.pkl')
 
 component_to_group_rdd = result_rdd.flatMap(lambda row: rowLambda(row))
 partitioned_rdd = component_to_group_rdd.partitionBy(num_partitions)
 partitioned_rdd.persist()
 
-def sumAll(partitionIter): 
-    yield sum([line[1][1] for line in partitionIter])
+def sumAll(partitionIter):
+    yield sum([tup[0] for tup in set([line[1][1:] for line in partitionIter])])
 
 group_nums_rdd = partitioned_rdd.mapPartitions(sumAll)
 
@@ -76,8 +79,8 @@ sam_only_partitions = partitioned_rdd.map(lambda line: line[1][0], preservesPart
 sam_only_partitions.persist()
 piped_output = sam_only_partitions.pipe("stringtie_mod")
 
-for line in piped_output.take(20):
-    print line
+gtfList = piped_output.glom().collect()
+gtfFileL = merge_gtfs(gtfList, group_nums_rdd.collect())
 
 #Gives a list of length num_partitions, of the sum total of unique reads corresponding to the partitions (in order) 
 readCountsByPartition = group_nums_rdd.collect()
