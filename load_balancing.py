080a8a9710d518a2b5ff9f7a15f6a76878cb91cf
Rohit Bhattacharya <rohit.bhattachar@gmail.com>
2016-12-19 23:41:12 -0500

    Added and modified greedy load balancing code

---
+++ load_balancing.py
@@ -0,0 +1,27 @@
+from graphframes import GraphFrame
+from cPickle import dump, load
+from collections import Counter
+from Queue import PriorityQueue
+from machine import Machine
+
+
+def create_machines(num_machines):
+    machines = PriorityQueue()
+    for i in range(num_machines):
+        new_machine = Machine(i)
+        machines.put(new_machine)
+    return machines
+
+
+def run_greedy_load_balancing(num_machines, component_read_counter):
+    machines = create_machines(num_machines)
+    component_read_counter_list = component_read_counter.most_common()
+    component_to_machine_id = {}
+    for component_id, read_count in component_read_counter_list:
+        component_reads = component_read_counter[component_id]
+        machine = machines.get()
+        machine.assign_component(component_id, read_count)
+        component_to_machine_id[component_id] = machine.get_id()
+        machines.put(machine)
+    #return list(machines.queue)
+    return component_to_machine_id
--- machine.py
+++ machine.py
@@ -1,18 +1,27 @@
 class Machine:
 
-    def __init__(self):
+    def __init__(self, id):
         self.load = 0
         self.reads = []
+        self.components = []
+        self.id = id
 
     def assign_reads(self, reads):
         self.reads.extend(reads)
         self.load += len(reads)
+
+    def assign_component(self, component, num_reads):
+        self.components.append(component)
+        self.load += num_reads
 
     def get_load(self):
         return self.load
 
     def clear_load(self):
         self.load = 0
+
+    def get_id(self):
+        return self.id
 
     def __cmp__(self, other):
         if self.load < other.get_load():
