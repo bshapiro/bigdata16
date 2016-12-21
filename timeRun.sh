c7fdf04d72a71f286af247811893c58fa82af022
rsherman15 <rsherman@g.hmc.edu>
2016-12-21 04:02:56 +0000

    fixed echo

--- timeRun.sh
+++ timeRun.sh
@@ -9,4 +9,4 @@ for i in `seq 1 5`; do
     { time spark-submit --packages graphframes:graphframes:0.2.0-spark1.5-s_2.10 --executor-memory $mem fullSparkSubmitRun.py -f /home/Rachel/ERR188044_chrX_sorted.bam -o /output/$num_cpu'CPU_'$num_node'Node_'$i/ -k $num_cpu ; } 2>> runTimes.txt
 done
 
-echo ${grep 'real' runTimes.txt | cut -f 2 | sed -e 's/[ms]/ /g' | awk '{print $1+$2/60}'}
+grep 'real' runTimes.txt | cut -f 2 | sed -e 's/[ms]/ /g' | awk '{print $1+$2/60}'
