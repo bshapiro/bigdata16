3a4a92791be132a695e0ebf3cec969265dc0a321
skovaka <skovaka@gmail.com>
2016-12-22 18:19:29 -0500

    Commented/cleaned gtf_merge and overlapParser.py

--- gtf_merge.py
+++ gtf_merge.py
@@ -3,77 +3,73 @@ import sys
 
 fpkm_re = re.compile("FPKM \"([\d\.]+)\"")
 tpm_re = re.compile("TPM \"([\d\.]+)\"")
-cov_re = re.compile("cov \"([\d\.]+)\"")
 
 
-def main(args):
-    files = ["../data/ERR188044_chrX_half1.gtf", "../data/ERR188044_chrX_half2.gtf"]
-    sizes = [1751527, 772507]
-
-    #files = ["test_1.gtf", "test_2.gtf"]
-    #sizes = [11995, 82509]
-    #sizes = [10908, 78849]
-    gtfs = [open(f).readlines() for f in files]
-    for line in merge_gtfs(gtfs, sizes):
-        print line
-
-
+##############################################################################
+# Takes a list of multiple contents of gtf files and number of mapped reads  #
+# used to generate each and returns the lines of a merged and normalized GTF #
+##############################################################################
 def merge_gtfs(gtfs, sizes):
+
     total_size = sum(sizes)
     fpkms = list()  # stores normalized FPKMs for each transcript
 
     file_rpks = list()  # stores sum of transcript RPK values (fpkm*size) for each file
-    #file_covs = list()
-
-    #total_fpkm = 0.0
-
+    
+    #Find and normalize FPKM value for each transcript
     for size, file_lines in zip(sizes, gtfs):
-        file_fpkm = 0.0
-        file_cov = 0.0
+        
+        file_fpkm = 0.0 # stores sum of FPKM value for each file
+
         for line in file_lines:
+
+            #Skip comments
             if line[0] == "#":
                 continue
-
+            
+            #Check if line has FPKM value
+            #Only lines for transcripts should match
             fpkm_m = fpkm_re.search(line)
             if fpkm_m:
-                #cov_m = cov_re.search(line)
-                #file_cov += float(cov_m.group(1))
 
+                #Calculate new FPKM
                 new_fpkm = (float(fpkm_m.group(1)) * size) / total_size
                 fpkms.append(new_fpkm)
                 file_fpkm += new_fpkm
-                #total_fpkm += new_fpkm
-
+        
+        #Store total RPK value for this file
         file_rpks.append(file_fpkm * total_size)
-        #file_covs.append(file_cov)
-
+    
     total_rpk = sum(file_rpks)
-
+    
+    #Stores output file lines
     out_lines = list()
-
+    
+    #Re-parse file to normalize TPM value (based on RPK)
     f = 0
     r = 0
     for file_lines in gtfs:
+
+        #Value used to normalize TPM
         tpm_factor = file_rpks[r] / total_rpk
         r += 1
 
         for line in file_lines:
+
+            #Skip comments
             if line[0] == "#":
                 continue
-
+            
+            #Find TPM value
             tpm_m = tpm_re.search(line)
             if tpm_m:
                 new_tpm = float(tpm_m.group(1)) * tpm_factor
-                #new_tpm = (1000000*fpkms[f])/total_fpkm
-                #new_tpm = float(tpm_m.group(1)) * file_covs[r-1] / sum(file_covs)
                 line = tpm_re.sub('TPM "%.6f"' % new_tpm, line)
                 line = fpkm_re.sub('FPKM "%.6f"' % fpkms[f], line)
                 f += 1
-
+            
+            #Store new line
             out_lines.append(line.strip())
 
     return out_lines
 
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
--- overlapParser.py
+++ overlapParser.py
@@ -3,9 +3,14 @@ import pysam
 MAX_GAP = 100
 MULTIMAP_FLAG = "NH"
 
-
+###################################################################
+# Creates groups of overlapping/neighboring reads from BAM file   #
+# Contains methods for iterating over groups and extracting reads #
+###################################################################
 class OverlapParser:
-
+    
+    # Constructor
+    # Takes filename for BAM file and maximum distance for reads to be in same group
     def __init__(self, filename=None, max_gap=100):
         self.group_size = 0
         self.unique_reads = 0
@@ -20,38 +25,47 @@ class OverlapParser:
         self.infile = pysam.AlignmentFile(filename, 'rb')
         self.reads_iter = self.infile.fetch()
         self.eof = False
-
+    
+    # Returns tuple of next group in list and any new edges between groups
     def next_group(self):
-
+        
+        #File contains no more reads
         if self.eof:
             return None, None
 
         group = None
         con_list = list()
-
+        
+        #Keep parsing reads until next group is found
         while group is None:
+
+            #Get next read
             try:
                 read = self.reads_iter.next()
             except StopIteration:
                 read = None
                 self.eof = True
-
+            
+            #Parse read
+            #Group will be None unless this read ends a group
             group, con = self.parse_read(read)
 
+            #Add new edge if any returned
             if con:
                 con_list.extend(con)
 
         return group, con_list
 
+    #Returns all SAM lines contained in a group
     def get_group(self, group):
         lines = list()
         _, ref, st, en = group
         for read in self.infile.fetch(ref, st, en):
             if not read.is_unmapped:
                 lines.append(read.tostring(self.infile))
-
         return lines
 
+    #Writes SAM/BAM lines from a list of groups to a file
     def write_groups(self, groups, filename):
         outfile = pysam.AlignmentFile(filename, "wb", template=self.infile)
 
@@ -64,46 +78,69 @@ class OverlapParser:
 
         outfile.close()
 
+    #Parses a SAM/BAM file read and returns group information
+    #Returns new group if read is outside of the current group (new group will be started)
+    #Also returns edges if a read is multimapped or mated with a read in another group
     def parse_read(self, read):
+
+        #Return values
         ovr_ret = con_ret = None
+
+        #Return current group if no read provided
         if not read:
             return (self.unique_reads, self.prev_ref, self.gmin, self.gmax), None
 
+        #No new group or connection if read unmapped
         if read.is_unmapped:
             return None, None
 
+        #Check if read is outside of current group
         if read.reference_start - self.gmax > self.max_gap or self.prev_ref != read.reference_name:
+
+            #Set group return value if it contains any reads
+            #Reset group number and read counts
             if self.group_size > 0:
                 ovr_ret = (self.unique_reads, self.prev_ref, self.gmin, self.gmax)
                 self.group_nm += 1
                 self.group_size = 0
                 self.unique_reads = 0
 
+            #Set new group bounds
             self.gmin = read.reference_start
             self.gmax = read.reference_end
+
+        #Update current group bounds
         else:
             self.gmax = max(self.gmax, read.reference_end)
 
+        #Add read to group size
         self.group_size += 1
 
+        #Update number of primary alignments
         if read.flag & 0x900 == 0:
             self.unique_reads += 1
 
+        #Update previous reference (chromosome) value
+        #Used to keep track of group bounds
         self.prev_ref = read.reference_name
 
+        #Check if read is multimapped - need to keep track of edges
         if read.get_tag(MULTIMAP_FLAG) > 1:
+
+            #Find current read edges if they exist
             neighbors = self.multimappers.get(read.query_name, None)
             if not neighbors:
                 neighbors = self.multimappers[read.query_name] = set([self.group_nm])
 
+            #Create new group edge if it doesn't exist
             if not self.group_nm in neighbors:
-
+                
+                #Create edge for each matching read
                 con_ret = list()
                 for g in sorted(neighbors):
                     if not (g, self.group_nm) in self.connections:
                         con_ret.append((g, self.group_nm))
                         self.connections.add((g, self.group_nm))
-
                 neighbors.add(self.group_nm)
 
         return ovr_ret, con_ret
