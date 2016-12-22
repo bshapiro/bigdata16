8031101c29d94694b9b80f48b02090f8939b97db
Benj Shapiro <benj.shapiro@gmail.com>
2016-12-22 16:54:25 -0500

    changing configureMaster to take any bam file as first argument

--- configureMaster.sh
+++ configureMaster.sh
@@ -1,3 +1,4 @@
 sudo cp tools/samtools /usr/bin/
 sudo cp tools/stringtie /usr/bin/
-samtools index /home/Rachel/ERR188044_chrX_sorted.bam
+# samtools index /home/Rachel/ERR188044_chrX_sorted.bam
+samtools index $1
